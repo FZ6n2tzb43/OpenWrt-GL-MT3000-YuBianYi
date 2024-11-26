96b4e72e973acc0dced1fcf0d83c586818b284d6
FZ6n2tzb43 <ranaixia69@gmail.com>
2024-11-26 12:25:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,11 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/OpenWrt24-GLMT3000-IPV4/g' package/base-files/files/bin/config_generate
+
+# Step 3: 修改默认时区
+# 将默认时区从 "UTC" 修改为 "CST-8"
+sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
+
+# Step 4: 增加时区名称设置为 "Asia/Shanghai"
+# 在时区设置后添加区域名称设置
+sed -i "/set system.\@system\[-1\].timezone='CST-8'/a\ \ \ \ set system.\@system\[-1\].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
